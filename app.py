6bf58b149e18a08cd76d961ad5ddb1fb6f374ec6
Freedomcoding101 <97307133+Freedomcoding101@users.noreply.github.com>
2024-01-29 17:35:34 +1100

    finished the view items function and it seems to be working

--- app.py
+++ app.py
@@ -3,6 +3,26 @@ from datetime import datetime
 import csv
 import time
 
+
+def app():
+    customer_input = input ('''
+    \n**Inventory Management System**
+    \rType "v" to view product
+    \rType "a" to add product
+    \rType "b" to backup inventory
+    \rType "exit" to exit
+    \r******************************  ''')
+
+    if customer_input.lower() == "v":
+        view_product()
+    elif customer_input.lower() == "a":
+        add_product()
+    elif customer_input.lower() == "b":
+        backup_file()
+    elif customer_input.lower() == "exit":
+        exit()
+    else: input("That is not a valid answer. Please re read the menu and enter an option from above. Press Enter to try again.  ")
+
 def add_csv():
     with open('inventory.csv') as csvfile:
         data = csv.reader(csvfile)
@@ -25,7 +45,6 @@ def add_csv():
             session.commit()
             
 
-
 def create_list():
     all_info = session.query(Product).all()
     product_list = []
@@ -43,10 +62,9 @@ def clean_price(price_string):
     try:cleaned_price = int(float(price_string.replace('$', '')) * 100)
     except:
         input('''
-        \n *****Price Error*****
+        \n******Price Error**********
         \rPlease input a valid price
-        \rPress Enter to try again
-        *******''')
+        \rPress Enter to try again''')
     else:
         return cleaned_price
 
@@ -58,8 +76,7 @@ def clean_quantity(quantity_string):
         \n *****Quantity Error*****
         \rEnter a numerical value
         \rusing WHOLE numbers!
-        \rPress Enter to try again
-        *******''')
+        \rPress Enter to try again''')
     else:
         return cleaned_quantity
         
@@ -68,6 +85,41 @@ def clean_date(date_string):
     cleaned_date = datetime.strptime(date_string, date_format).date()
     return cleaned_date
 
+def view_product():
+    products = session.query(Product.product_id, Product.product_name).all()
+    for product in products:
+        print(f"{product.product_id}: {product.product_name}")
+    print("\n")
+    app_running = True
+    while app_running:
+        try:
+            product_number = input('''Which product would you like more information about??
+                                    \rType 'menu' to go back!
+                                    \rType 'print' to reprint list!
+                                    \rOr enter number HERE ----- >  ''')
+
+            if product_number.lower() == "menu":
+                app_running = False
+            elif product_number.lower() == "print":
+                products = session.query(Product.product_id, Product.product_name).all()
+                for product in products:
+                    print(f"{product.product_id}: {product.product_name}")
+                print("\n")
+            else:
+                query = session.query(Product).filter(Product.product_id == product_number).first()
+                print(f"\n**** There are {query.product_quantity} units of {query.product_name} in stock at a price of ${query.product_price / 100:.2f} each! This is as of {query.date_updated} ****\n")
+        except: 
+            print("That is not a valid option, please read the list/menu above and try again\n")
+    app()
+
+def add_product():
+    add_name = input("What is the name of your product? Example: Juice - V8 Splash.  ")
+    add_price = input("What is the current price of this product? Example : $5.23  ")
+    add_quantity = input("How many units are you adding to inventory? Example : 32  ")
+
+    add_product = Product(product_name=(add_name), product_price = clean_price(add_price), product_quantity = clean_quantity(add_quantity), date_updated = (datetime.now))
+    session.add(add_product)
+    session.commit()
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
